a969545c37fe3e6baa84d55a83c4ac4fa41f71f8
Bigserver Development VM Pütti <cebit-fan@web.de>
2022-03-31 10:49:11 +0000

    adding zmq socket

--- examples/tradeclient/zmqTest.cpp
+++ examples/tradeclient/zmqTest.cpp
@@ -36,7 +36,7 @@ static void s_catch_signals (void)
 }
 
 
-const std::string TOPIC = "blocks";
+const std::string TOPIC = "Bittrex";
 
 void startPublisher()
 {
@@ -47,6 +47,7 @@ void startPublisher()
     zmq::message_t msg(3);
     zmq::message_t topic(4);
     for(int i = 0; i < 10; i++) {
+
         memcpy(topic.data(), TOPIC.data(), TOPIC.size()); // <= Change your topic message here
         memcpy(msg.data(), "abc", 3);
         try {
@@ -65,7 +66,8 @@ void startSubscriber()
 {
     zmq::context_t zmq_context(1);
     zmq::socket_t zmq_socket(zmq_context, ZMQ_SUB);
-    zmq_socket.connect("tcp://192.168.20.152:5600");
+    // zmq_socket.connect("tcp://192.168.20.152:5600");
+    zmq_socket.connect("tcp://192.168.20.104:4242");
 
     zmq::socket_t killer_socket(zmq_context, ZMQ_PAIR); // This socket is used to terminate the loop on a signal
     killer_socket.bind("ipc://killmebaby");
@@ -94,9 +96,33 @@ void startSubscriber()
                 // start parsing msg as json
                 auto data = json::parse(std::string(static_cast<char*>(msg.data()), msg.size()));
                 // explicit conversion to string
-                std::cout << "updateid: " << data["update"]["id"] << std::endl;
-                std::string s = data.dump(); 
+                // std::cout << "updateid: " << data["update"]["id"] << std::endl;
+                std::string s = data.dump();
 
+
+                // Start processing the msg
+
+                // {"action": "PLACE_ORDER", "symbol": "WAVES-BTC", "side": "SELL", "ordertype": "LIMIT", "orderQty": 3.0, "price": 54.44534, "orderId":"fc9bf4a3-7a2a-4673-9e64-7b4eb9562a11"}
+                if (data["action"] == "PLACE_ORDER") {
+                    std::string symbol = data["symbol"];
+                    std::string side = data["side"];
+                    std::string ordertype = data["ordertype"];
+                    std::string orderId = std::string();
+                    long orderQty_raw = data["orderQty"];
+                    double price_raw = data["price"];
+
+                    if (data.contains("orderId")) {
+                        orderId = data["orderId"];
+                    }
+                    // long orderQty = strtol(orderQty_raw.c_str(), nullptr, 10);
+                    // double price = strtod(price_raw.c_str(), nullptr);
+
+                    std::cout << "PLACE ORDER: " << orderId << std::endl;
+
+                }
+                else if (data["action"] == "CANCEL_ORDER") {
+                    std::string orderId = data["orderId"];
+                }
 
             }
         }
